a3ec93a039e08141bae6b67a9075397f1e1bef15
Jason Browning <jason@riverdartgallery.co.uk>
2019-03-20 23:06:33 +0000

    First half of setup

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
 
 const app = express();
+
+//Init middleware
+// app.use(logger);
+
+//Body PArser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Members api routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
