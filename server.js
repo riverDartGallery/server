6c555bb48d84630cfe65d87e7f8f1e6dca7111bb
Jason Browning <jason@riverdartgallery.co.uk>
2019-03-21 19:29:56 +0000

    handlebars

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const members = require('./Members');
 // const logger = require('./middleware/logger');
 
 const app = express();
 
 //Init middleware
+
+// Logger
 // app.use(logger);
 
-//Body Parser middleware
+//Handlebars Middleware
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
+
+//Homepage Route
+app.get('/', (req, res) => res.render('index', {
+  title: 'Member App',
+  members
+}));
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, "public")));
 
-//Members api routes
-app.use('/api/members', require('./routes/api/members'));
+//Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
